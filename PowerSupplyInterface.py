2ffc9c24a3ffc6716beaffb0a1eba562a451a19b
ibeckermayer <ibeckermayer@gmail.com>
2017-06-23 10:45:39 -0700

    implemented channel on and off feature. Next steps are written down in notebook

--- PowerSupplyInterface.py
+++ PowerSupplyInterface.py
@@ -4,14 +4,15 @@ import time
 class PowerSupplyInterface:
 	# initialize PSI connection with the proper host ip and port number, voltages and currents to 0	
 	def __init__(self):
+		self.SLEEP_TIME = 0.5 # telnet is ancient and therefore needs some time to send messages
 		self.host = "142.103.235.210" # Set using manual control
 		self.port = "5024"	      # As described in user manual
 		self.current1 = None
 		self.voltage1 = None		
 		self.current2 = None
 		self.voltage2 = None
-		self.SLEEP_TIME = 0.5 # telnet is ancient and therefore needs some time to send messages
-		
+		self.chan1status = None # either ON or OFF
+		self.chan2status = None # either ON or OFF
 		#initialize connection
 		self.tn = telnetlib.Telnet(self.host, self.port)
 		time.sleep(self.SLEEP_TIME)
@@ -24,7 +25,6 @@ class PowerSupplyInterface:
 	# set voltage on channel 1 or 2
 	def set_voltage(self, chan, voltage):
 		if chan==1:
-			print "channel1 voltage!"
 			self.voltage1 = voltage
 			self.tn.write("VOLT " + str(self.voltage1) + "\r\n")
 			time.sleep(self.SLEEP_TIME)
@@ -44,15 +44,37 @@ class PowerSupplyInterface:
 			self.tn.write("CURR2 " + str(self.current2) + "\r\n")
 			time.sleep(self.SLEEP_TIME)
 
+	# turn channel 1 or 2 ON
+	def turn_channel_on(self, chan):
+		if chan==1:
+			self.tn.write("OUT ON\r\n")
+			time.sleep(self.SLEEP_TIME)
+		if chan==2:
+			self.tn.write("OUT2 ON\r\n")
+			time.sleep(self.SLEEP_TIME)
+
+	# turn channel 1 or 2 OFF
+	def turn_channel_off(self, chan):
+		if chan==1:
+			self.tn.write("OUT OFF\r\n")
+			time.sleep(self.SLEEP_TIME)
+		if chan==2:
+			self.tn.write("OUT2 OFF\r\n")
+			time.sleep(self.SLEEP_TIME)
+
+
 	# close the telnet connection
 	def close(self):
 		self.tn.close()
 
 if __name__ == "__main__":
 	psi = PowerSupplyInterface()
-	psi.set_voltage(1,6)
-	psi.set_voltage(2,10)
-	psi.set_current(1,10.2)
-	psi.set_current(2,10.2)
+	psi.set_voltage(1,7)
+	psi.set_voltage(2,7)
+	psi.set_current(1,10)
+	psi.set_current(2,10)
+	#psi.turn_channel_on(2)
+	#time.sleep(2)
+	#psi.turn_channel_off(2)
 	psi.close()
 
