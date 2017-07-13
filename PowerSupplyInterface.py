aaf88a4e429f8ecd2fe1f0a8d226959a1397bdef
ibeckermayer <ibeckermayer@gmail.com>
2017-07-13 16:12:41 -0700

    updated all the old functions with new methods of sending commands and read_until that make the code much cleaner. Next step ought to be to write "real" unit tests for those old functions, as a matter of good coding practice and to test that my new configuration works

--- PowerSupplyInterface.py
+++ PowerSupplyInterface.py
@@ -14,93 +14,131 @@ class BKPRECISION9173:
         self.voltageSetting2 = None
         self.chan1status = None  # either ON or OFF
         self.chan2status = None  # either ON or OFF
-        self.executing = false  #
+        self.executing = False  # may be necessary later to avoid data acquisition / command overlap
 
-    # initialize the connection and read until the first period
     def init_connection(self):
+        """
+initialize the telnet connection
+        """
         self.tn = telnetlib.Telnet(self.host, self.port)
         time.sleep(self.SLEEP_TIME)
 
         # read until the first period (telnet connection causes an intro statement to print out)
         # this is necessary since we will use '.' to identify the voltage and current query responses
-        self.tn.read_until('.', 5)
-        time.sleep(self.SLEEP_TIME)
+        self.read_until('.')
 
-    # set voltage on channel 1 or 2
+    def send_command(self, command):
+        """
+function for sending commands via telnet connection
+        :param command: string
+        """
+        self.tn.write(command + "\r\n")  # always need "\r\n" at the end to actually send
+        time.sleep(self.SLEEP_TIME)  # always want to sleep after to ensure command has time to be received
+
+    def read_until(self, string):
+        """
+Function for reading back until a given string, returns everything read back
+        :param string: string
+        :return: string
+        """
+        return_string = self.tn.read_until(string, 5)
+        time.sleep(self.SLEEP_TIME)  # always sleep after to make sure timing is ok
+        return return_string
+
     def set_voltage(self, chan, voltage):
+        """
+set voltage on channel 1 or 2
+        :param chan: int (1 or 2)
+        :param voltage: float or int
+        """
         if chan == 1:
             self.voltageSetting1 = voltage
-            self.tn.write("VOLT " + str(self.voltageSetting1) + "\r\n")
-            time.sleep(self.SLEEP_TIME)
+            self.send_command("VOLT " + str(self.voltageSetting1))
         if chan == 2:
             self.voltageSetting2 = voltage
-            self.tn.write("VOLT2 " + str(self.voltageSetting2) + "\r\n")
-            time.sleep(self.SLEEP_TIME)
+            self.send_command("VOLT2 " + str(self.voltageSetting2))
 
     # set current on channel 1 or 2
     def set_current(self, chan, current):
+        """
+set current on channel 1 or 2
+        :param chan: int (1 or 2)
+        :param current: float or int
+        """
         if chan == 1:
             self.currentSetting1 = current
-            self.tn.write("CURR " + str(self.currentSetting1) + "\r\n")
-            time.sleep(self.SLEEP_TIME)
+            self.send_command("CURR " + str(self.currentSetting1))
         if chan == 2:
             self.currentSetting2 = current
-            self.tn.write("CURR2 " + str(self.currentSetting2) + "\r\n")
-            time.sleep(self.SLEEP_TIME)
+            self.send_command("CURR2 " + str(self.currentSetting2))
 
-    # turn channel 1 or 2 ON
     def turn_channel_on(self, chan):
+        """
+turn channel 1 or 2 ON
+        :param chan: int (1 or 2)
+        """
         if chan == 1:
-            self.tn.write("OUT ON\r\n")
-            time.sleep(self.SLEEP_TIME)
+            self.send_command("OUT ON")
         if chan == 2:
-            self.tn.write("OUT2 ON\r\n")
-            time.sleep(self.SLEEP_TIME)
+            self.send_command("OUT2 ON")
 
-    # turn channel 1 or 2 OFF
     def turn_channel_off(self, chan):
+        """
+turn channel 1 or 2 OFF
+        :param chan: int (1 or 2)
+        """
         if chan == 1:
-            self.tn.write("OUT OFF\r\n")
-            time.sleep(self.SLEEP_TIME)
+            self.send_command("OUT OFF")
         if chan == 2:
-            self.tn.write("OUT2 OFF\r\n")
-            time.sleep(self.SLEEP_TIME)
+            self.send_command("OUT2 OFF")
 
-    # close the telnet connection
     def close(self):
+        """
+close the telnet connection
+        """
         self.tn.close()
 
-    def measure_current(self, chan):
-        """
-Function to measure the current at the output of one of the channels.
-Note that this is distinct from get_set_current, which checks what the
-current is set to be at (rather than what its actually at).
-        :param chan: int (1 or 2)
-        :return: float
-        """
-        if chan == 1:
-            self.tn.write("MEAS CURR?\r\n")
-        if chan == 2:
-            self.tn.write("MEAS CURR2?\r\n")
+#     def measure_current(self, chan):
+#         """
+# Function to measure the current at the output of one of the channels.
+# Note that this is distinct from get_set_current, which checks what the
+# current is set to be at (rather than what its actually at).
+#         :param chan: int (1 or 2)
+#         :return: float
+#         """
+#         if chan == 1:
+#             self.send_command("MEAS CURR?")
+#         if chan == 2:
+#             self.send_command("MEAS CURR2?")
+#
+#         before_decimal = self.read_until(".")
+#         after_decimal = self.read_until("\r")[0:3]
+#
+#         if before_decimal[-3] == "\n":  # check if it's 1 digit before the decimal
+#             before_decimal = before_decimal[-2]
+#         else:
+#             before_decimal = before_decimal[-3:-1]
+#
+#         return float(before_decimal + "." + after_decimal)
+#
+#     def measure_voltage(self, chan):
+#         """
+# Function to measure the voltage at the output of one of the channels.
+# Note that this is distinct from get_set_voltage, which checks what the
+# voltage is set to be at (rather than what its actually at).
+#         :param chan: int (1 or 2)
+#         :return: float
+#         """
 
-        time.sleep(self.SLEEP_TIME)
 
-        before_decimal = self.tn.read_until(".", 5)
-        after_decimal  = self.tn.read_until("\r")[0:3]
 
-        if before_decimal[-3] == "\n": # check if it's 1 digit before the decimal
-            before_decimal = before_decimal[-2]
-        else:
-            before_decimal = before_decimal[-3:-1]
 
-        return float(before_decimal + "." + after_decimal)
-
-    # TODO: measure_voltage(), get_set_current(), get_set_voltage()
-    # TODO: make BKPRECISION9173 it's own .py file (look up how to import self made modules in python)
-    # TODO: make a real test script, with unit tests and real ways to fail
+        # TODO: measure_voltage(), get_set_current(), get_set_voltage()
+        # TODO: make BKPRECISION9173 it's own .py file (look up how to import self made modules in python)
+        # TODO: make a real test script, with unit tests and real ways to fail
         # for the sake of good coding style
         # for the sake of learning how to use this
-
+        # TODO: make some functions for initialization, which measure and set the fields
 
 
 if __name__ == "__main__":
