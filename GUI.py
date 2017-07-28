4c62d7db39607db4c2320c6485946c3722107e63
ibeckermayer <ibeckermayer@gmail.com>
2017-07-28 15:49:33 -0700

    Unit Tests passed, started working on the GUI

---
+++ GUI.py
@@ -0,0 +1,37 @@
+"""
+Code modified largely from: http://zetcode.com/gui/tkinter/
+"""
+
+import Tkinter as tk
+
+
+class UI(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.initUI()
+
+    def initUI(self):
+        self.master.title("Power Supply Interface")  # master gives access to the root window (Tk)
+        self.pack(fill=tk.BOTH, expand=1)  # put our Frame on entire Tk window
+        self.centerWindow()
+
+    def centerWindow(self):
+        w = self.master.winfo_screenwidth()/1.1
+        h = self.master.winfo_screenheight()/1.3
+
+        sw = self.master.winfo_screenwidth()
+        sh = self.master.winfo_screenheight()
+
+        x = (sw - w) / 2
+        y = (sh - h) / 2
+        self.master.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+
+def main():
+    root = tk.Tk()
+    ui = UI()
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
